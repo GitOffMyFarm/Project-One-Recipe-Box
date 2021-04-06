9b61e70cfb3591ab5460bf7fdb838e4d30979ecc
Chris Z <c.zacharias2021@gmail.com>
2021-04-06 18:50:36 -0400

    updating branch

---
+++ assets/js/homepage.js
@@ -0,0 +1,23 @@
+var getRecipe = document.getElementById("#get-recipe-btn btn")
+getRecipe.addEventListener("click", searchRecipes)
+
+function searchRecipes() {
+    fetch("https://edamam-edamam-nutrition-analysis.p.rapidapi.com/api/nutrition-data?ingr=1%20large%20apple", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "717d918a52msh8b9e2dec05bd4f7p13657ejsn9f3cab4a73b0",
+		"x-rapidapi-host": "edamam-edamam-nutrition-analysis.p.rapidapi.com"
+	}
+})
+.then(response => response.json())
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+})
+}
+
+function renderRecipe() {
+    var recRendered = 
+}
